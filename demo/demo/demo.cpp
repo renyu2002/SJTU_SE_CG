1423e611372e67a5822cdbca30798e9a64793f89
0727nr <101614558+0727nr@users.noreply.github.com>
2024-09-27 16:15:48 +0800

    demo

---
+++ demo/demo/demo.cpp
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<imgui.h>
+#include<imgui_impl_glfw.h>
+#include<imgui_impl_opengl3.h>
+#include <chrono>
+#include "util.h"
+
+float quad[] = {
+	0.0,0.0,1.0,0.0,0.0,
+	0.0,0.5,0.0,1.0,0.0,
+	0.5,0.0,0.0,0.0,1.0
+};
+unsigned int VAO, VBO;
+
+std::chrono::steady_clock::time_point start,end;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+void updateUI()
+{
+	bool imguiactive = true;
+	ImGui::NewFrame();
+	ImGui::Begin("information", &imguiactive, ImGuiWindowFlags_MenuBar);
+	ImGui::SetWindowPos(ImVec2(2, 2));
+	ImGui::SetWindowSize(ImVec2(400, 200));
+	const GLubyte* vendor = glGetString(GL_VENDOR);
+	const GLubyte* renderer = glGetString(GL_RENDER);
+	const GLubyte* version = glGetString(GL_VERSION);
+	std::chrono::duration<double> duration = end - start;
+	double microseconds = duration.count() * 1000000;
+	int framerate = 1000000 / std::max(microseconds,1.0);
+	
+
+	ImGui::Text("vendor:%s,renderer:%s,version:%s", vendor, renderer,version);
+	ImGui::Text("framerate:%d", framerate);
+	ImGui::End();
+	ImGui::Render();
+}
+void clearfb()
+{
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+void render()
+{
+
+}
+void initprimitiveinfo()
+{
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(float) * 5, quad, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+}
+int main()
+{
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "SJTU_SE_CG", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	initprimitiveinfo();
+
+	Shader shader("test.vert", "test.frag");
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+	start= std::chrono::high_resolution_clock::now();
+	end= std::chrono::high_resolution_clock::now();
+	while (!glfwWindowShouldClose(window))
+	{
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		updateUI();
+		start= std::chrono::high_resolution_clock::now();
+
+		processInput(window);
+		clearfb();
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+		// core render segment
+		{
+			shader.use();
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+		}
+		
+		// Start the Dear ImGui frame
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		//swap chain related
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		end = std::chrono::high_resolution_clock::now();
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ demo/readme.md
@@ -0,0 +1,30 @@
+# demo
+
+basic homework architecture with minimum code
+
+
+# request
+* glfw
+* glad
+* imgui(recommend to install for easy ui operation)
+* assimp(optional, for model operation)
+* eigen(optional, for math calculation)
+
+# environment install using vcpkg
+vcpkg是微软推出的一款库管理软件,也是我觉得目前较为简易的windows+visual studio工作环境下的依赖配置软件
+
+安装和使用说明详见 [microsoft vcpkg](https://github.com/microsoft/vcpkg)
+
+    vcpkg integrate install
+    vcpkg install glfw3:x64-windows glad:x64-windows imgui[core,opengl3-binding,glfw-binding]:x64-windows
+    vcpkg install assimp:x64-windows eigen3:x64-windows
+
+if you installed successful, you can see the following image
+![](res.png)
+
+# some attention
+* pay attention to the relative path of your resource file when you upload your executable binary
+* check if there is any dll missing befor you packing your binaries to ensure that the homework can run on our score computer
+
+# other platform
+for platform that is not windows, i recommend you to rely on the cmake to build your own proj. you can reference to the cmake proj like [learnopengl](https://github.com/JoeyDeVries/LearnOpenGL), or some simpler ones like [OpenGLSampleCmake](https://github.com/Shot511/OpenGLSampleCmake)
